fc902237a7ef6ce68f4fe7f6b93068a3efb25bf6
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-08-02 12:35:09 -0400

    using css

--- students.js
+++ students.js
@@ -8,6 +8,12 @@ app.use(express.json())
 
 let students = ["Bob", "Samantha", "Lily", "Grace", "Jim"]
 
+app.get("/movies/:movietitle/:director",  (req,res)=> {   // for http://localhost:4000/movie/matrix ... If i'm searching in the mongoDB database
+  const movietitle = req.params.movietitle // get the actual movie title (everything to the right of "/movies/"
+  const director = req.params.director
+  console.log(`Looking for movie ${movietitle}`) // Show me (to prove the obvios) what the user passed to me.
+  const query = {title: {'$regex': movietitle, '$options': 'i'}}}); 
+
 app.get("/", (req, res) => {
   let output = "<html><body><ul>" //start
 
